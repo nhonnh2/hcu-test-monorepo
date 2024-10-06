6164ded3ce6f4cba67debb80be9c22339d6655fe
Nguyen Huu Nhon <huunhon099@gmail.com>
2024-10-06 18:39:12 +0700

    setup utils and axiosInstance

--- apps/task-management/src/app/app.tsx
+++ apps/task-management/src/app/app.tsx
@@ -1,9 +1,8 @@
-import NxWelcome from './nx-welcome';
 import { GlobalLayout } from '@hcu-test-monorepo/shared-ui';
 export function App() {
   return (
     <GlobalLayout>
-      <NxWelcome title="task-management" />
+      <div>Hello</div>
     </GlobalLayout>
   );
 }
---
+++ libs/shared/utils/src/lib/axios.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+// config
+
+// ----------------------------------------------------------------------
+
+const axiosInstance = axios.create({
+  baseURL: process.env.PUBLIC_API_ENDPOINT,
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) =>
+    Promise.reject(
+      (error.response && error.response.data) || 'Something went wrong'
+    )
+);
+
+export default axiosInstance;
